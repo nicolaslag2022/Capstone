094d2b2d5a3364d30c752de228f8c1fdb645d5dd
nicolaslag2022 <nick.goforth@gmail.com>
2022-12-05 16:51:55 -0600

    capstone spa update

--- components/views/Home.js
+++ components/views/Home.js
@@ -1,18 +1,20 @@
 import html from "html-literal";
+import shdlogo from "../../assets/images/shdlogo.jpg";
+import homepagepic from "../../assets/images/homepagepic.jpg";
 
 export default () => html`
   <header>
-      <img src="shdlogo.jpg" class="image1" />
+      <img src=${shdlogo} class="image1" />
       <h1>Welcome to Smokin Hot Dad BBQ</h1>
-      <h2>Your Home for great BBQ recipes and Supplies!</h2>
+      <h2>Your Home for great BBQ recipes!</h2>
     </header>
     <nav>
       <ul class="main_menu">
         <li><a href="index.html">Home</a></li>
         <li><a href="Aboutme.html">Bio</a></li>
-        <li><a href="Shop.html">Shop</a></li>
+        <li><a href="Flavorpairing.html">Flavor Pairing</a></li>
       </ul>
-      <img src="IMG_1481.jpeg" class="image2" />
+      <img src=${homepagepic} width="1500" height="800"/>
     </nav>
   </div>
 `;
