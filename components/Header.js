12d748288155bd32cb346aeba0df2e924558f6e6
nicolaslag2022 <nick.goforth@gmail.com>
2022-12-09 13:50:49 -0600

    capstone spa work

--- components/Header.js
+++ components/Header.js
@@ -1,7 +1,7 @@
 import html from "html-literal";
 export default state => html`
   <header style="background:#FF4633; padding:10px 20px; border-radius: 20px">
-    <h1>Welcome to Smokin Hot Dad BBQ</h1>
+    <h1><b>Welcome to Smokin Hot Dad BBQ</b></h1>
     <h2>Your Home for great BBQ recipes!</h2>
   </header>
 `;
--- components/views/Home.js
+++ components/views/Home.js
@@ -5,5 +5,10 @@ export default () => html`
     <nav>
       <img src=${homepagepic} width="1500" height="800" />
     </nav>
+    <h3>
+      The weather in ${state.weather.city} is ${state.weather.description}.
+      Temperature is ${state.weather.temp}F, and it feels like
+      ${state.weather.feelsLike}F.
+    </h3>
   </div>
 `;
