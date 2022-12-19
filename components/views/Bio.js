1112650297864c7ff9ed6ebcdf3dc7e877e6963d
nicolaslag2022 <nick.goforth@gmail.com>
2022-12-19 16:16:26 -0600

    capstone table add

--- components/views/Bio.js
+++ components/views/Bio.js
@@ -1,16 +1,33 @@
 import html from "html-literal";
-import aboutmepic from "../../assets/images/aboutmepic.jpg";
+import gallery1 from "../../assets/images/gallery1.jpg";
+import gallery2 from "../../assets/images/gallery2.jpg";
+import gallery3 from "../../assets/images/gallery3.jpg";
+import gallery4 from "../../assets/images/gallery4.jpg";
+import gallery5 from "../../assets/images/gallery5.jpg";
+import gallery6 from "../../assets/images/gallery6.jpg";
+import gallery7 from "../../assets/images/gallery7.jpg";
+import gallery8 from "../../assets/images/gallery8.jpg";
 export default () => html`
-  <div id="biopage">
-    <p style="color:white">
-      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-      tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-      veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-      commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
-      velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
-      cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id
-      est laborum.
+  <section>
+    <div class="bioheading">
+    <h1>Bio</h1>
+    <p>
+    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
     </p>
-    <img src=${aboutmepic} />
-  </div>
+    </div>
+    <div>
+      <div class="gallery-container">
+      <div class="thumbnails"></div>
+      <div class="slides">
+        <div><img src=${gallery1} /></div>
+        <div><img src=${gallery2} /></div>
+        <div><img src=${gallery3} /></div>
+        <div><img src=${gallery4} /></div>
+        <div><img src=${gallery5} /></div>
+        <div><img src=${gallery6} /></div>
+        <div><img src=${gallery7} /></div>
+        <div><img src=${gallery8} /></div>
+      </div>
+    </div>
+  </section>
 `;
--- components/views/Flavorpairing.js
+++ components/views/Flavorpairing.js
@@ -1,28 +1,37 @@
 import html from "html-literal";
-import gallery1 from "../../assets/images/gallery1.jpg";
-import gallery2 from "../../assets/images/gallery2.jpg";
-import gallery3 from "../../assets/images/gallery3.jpg";
-import gallery4 from "../../assets/images/gallery4.jpg";
-import gallery5 from "../../assets/images/gallery5.jpg";
-import gallery6 from "../../assets/images/gallery6.jpg";
-import gallery7 from "../../assets/images/gallery7.jpg";
-import gallery8 from "../../assets/images/gallery8.jpg";
+import cow from "../../assets/images/cow.jpg";
+import pig from "../../assets/images/pig.jpg";
+import chicken from "../../assets/images/chicken.jpg";
 export default () => html`
   <section>
-    <h2>Flavor Pairing</h2>
-    <div>
-      <div class="gallery-container">
-      <div class="thumbnails"></div>
-      <div class="slides">
-        <div><img src=${gallery1} /></div>
-        <div><img src=${gallery2} /></div>
-        <div><img src=${gallery3} /></div>
-        <div><img src=${gallery4} /></div>
-        <div><img src=${gallery5} /></div>
-        <div><img src=${gallery6} /></div>
-        <div><img src=${gallery7} /></div>
-        <div><img src=${gallery8} /></div>
-      </div>
+    <h2 class="flavorheading">Flavor Pairing</h2>
+    <div class="box">
+      <table class="table" style="width:100%">
+        <tr>
+          <td>Meat Type</td>
+          <th scope="col">Mesquite</th>
+          <th scope="col">Hickory</th>
+          <th scope="col">Pecan</th>
+        </tr>
+        <tr>
+          <td><img src=${cow} /></td>
+          <th scope="row">&#10004</th>
+          <th scope="row">&#10004</th>
+          <th scope="row">&#10004</th>
+        </tr>
+        <tr>
+          <td><img src=${pig} /></td>
+          <th scope="row"></th>
+          <th scope="row">&#10004</th>
+          <th scope="row">&#10004</th>
+        </tr>
+        <tr>
+          <td><img src=${chicken} /></td>
+          <th scope="row">&#10004</th>
+          <th scope="row">&#10004</th>
+          <th scope="row">&#10004</th>
+        </tr>
+      </table>
     </div>
   </section>
 `;
