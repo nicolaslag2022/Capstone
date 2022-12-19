75b19957711931b42222b50f705535b9bb4173bb
nicolaslag2022 <nick.goforth@gmail.com>
2022-12-19 11:39:07 -0600

    mongodb install and deploy

--- server/app.js
+++ server/app.js
@@ -1,15 +1,60 @@
-// 'Import' the Express Module Instead of http
+// 'Import' the Express module instead of http
 const express = require("express");
-// Initialize the express application
+// Initialize the Express application
+const dotenv = require("dotenv");
+const mongoose = require("mongoose");
+
+dotenv.config();
+
+const PORT = process.env.PORT || 4040; // we use || to provide a default value
+
+// let PORT = 4040;
+
+// if (process.env.PORT) {
+//   PORT = process.env.PORT;
+// }
+
 const app = express();
-// Request handlers go here
+
+const db = mongoose.connection;
+
+mongoose.connect(process.env.MONGODB);
+
+db.on("error", console.error.bind(console, "Connection Error:"));
+db.once(
+  "open",
+  console.log.bind(console, "Successfully opened connection to Mongo!")
+);
+
+const logging = (request, response, next) => {
+  console.log(`${request.method} ${request.url} ${Date.now()}`);
+  next();
+};
+
+app.use(express.json());
+app.use(logging);
+
 app.get("/status", (request, response) => {
-  // Create the headers for response by default 200
-  // Create response body
-  // End and return the response
-  response.send(JSON.stringify({ message: "Service healthy" }));
+  response.status(200).json({ message: "Service healthy" });
 });
 
-// Tell Express app to start listening
-// Running on 4040
-app.listen(4040, () => console.log("Listening on port 4040"));
+app.get("/users/:id", (request, response) => {
+  // express adds a "params" Object to requests
+  const id = request.params.id;
+  // handle GET request for post with an id of "id"
+  response.send(JSON.stringify({ user_id: id }));
+});
+
+app.post("/add", (request, response) => {
+  const num1 = request.body.numberOne;
+  const num2 = request.body.numberTwo;
+  const responseBody = {
+    sum: num1 + num2
+  };
+  response.json(responseBody);
+});
+
+// Tell the Express app to start listening
+// Let the humans know I am running and listening on 4040
+
+app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
